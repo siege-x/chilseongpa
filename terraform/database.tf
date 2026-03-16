20b1013346bc4d4c241fc333e10f630c567d93ce
siege <gbu030631@gmail.com>
2026-03-16 11:42:17 +0900

    Create database.tf

---
+++ terraform/database.tf
@@ -0,0 +1,31 @@
+# ==============================================================================
+# [database.tf] 백엔드 앱의 데이터를 저장할 구글 관리형 DB를 생성합니다.
+# 오토스케일링을 지원하는 Auth Proxy 아키텍처를 적용했습니다.
+# ==============================================================================
+
+resource "google_sql_database_instance" "primary_db" {
+  name             = "hybrid-primary-db"
+  database_version = "MYSQL_8_0" # 최신 MySQL 8.0 엔진 사용
+  region           = var.region
+
+  settings {
+    # DB 체급 설정: 부하 테스트를 견딜 수 있도록 vCPU 2개, RAM 7.5GB 할당
+    tier = "db-custom-2-7680" 
+    
+    ip_configuration {
+      # 💡 핵심: Auth Proxy(IAM 인증)가 다이렉트로 찾아올 수 있도록 Public IP 활성화
+      # (IP 방화벽 검사를 하지 않으므로 authorized_networks 설정이 필요 없습니다!)
+      ipv4_enabled = true 
+    }
+  }
+  
+  # 테스트 환경이므로 쉽게 지웠다 만들 수 있도록 삭제 보호 기능 끄기
+  deletion_protection = false 
+}
+
+# DB 접속용 기본 루트 사용자 생성
+resource "google_sql_user" "root_user" {
+  name     = "root"
+  instance = google_sql_database_instance.primary_db.name
+  password = var.db_password # variables.tf에서 주입받은 비밀번호 사용
+}
