4a85bf2c0cc998e634b21af2db4b06be50518a45
siege <gbu030631@gmail.com>
2026-03-20 11:22:41 +0900

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -35,3 +35,19 @@ resource "google_secret_manager_secret_version" "db_proxy_key_version" {
   secret_data = base64decode(google_service_account_key.db_proxy_sa_key.private_key)
   depends_on  = [google_service_account_key.db_proxy_sa_key]
 }
+
+# Prometheus 모니터링 서버의 접근을 허용하는 방화벽 규칙
+resource "google_compute_firewall" "allow_node_exporter" {
+  name    = "allow-node-exporter"
+  network = google_compute_network.vpc_network.name # 성호 님 VPC 이름으로 확인하세요!
+
+  allow {
+    protocol = "tcp"
+    ports    = ["9100"]
+  }
+
+  # 희정님(모니터링 서버)의 IP만 허용하는 게 보안상 가장 좋지만,
+  # 현재 팀원 전체 내부 통신을 위해 일단 0.0.0.0/0 또는 내부망 대역을 넣습니다.
+  source_ranges = ["0.0.0.0/0"] 
+  target_tags   = ["k3s-node"] # 성호 님 VM에 설정된 태그와 맞춰야 합니다!
+}
